3703e544e3286714564b9fb794e807489d11a8c4
jguyet <gygydu19@hotmail.fr>
2024-03-25 00:20:19 +0100

    remove cors

--- server.js
+++ server.js
@@ -15,28 +15,28 @@ var express = require('express'),
 //Json BodyParser
 /////////////////////////////////////////////////////////////////////////
 
-var allowedOrigins = ['*'];
+// var allowedOrigins = ['*'];
 
-app.use(cors({
-    origin: function(origin, callback) {
-      // allow requests with no origin 
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf('*') === -1 && allowedOrigins.indexOf(origin) === -1) {
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
-        console.log('Origin blocked by CORS policy ', origin);
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-}));
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
+// app.use(cors({
+//     origin: function(origin, callback) {
+//       // allow requests with no origin 
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf('*') === -1 && allowedOrigins.indexOf(origin) === -1) {
+//         var msg = 'The CORS policy for this site does not ' +
+//                   'allow access from the specified Origin.';
+//         console.log('Origin blocked by CORS policy ', origin);
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     }
+// }));
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   next();
+// });
 
 
 JSON.parseAll = (v) => { try { return typeof v == "string" ? JSON.parse(v) : v; } catch (x) { return v; } }
